d548df6f2dfed685621702fdfbfac877216dd9ba
Alton <alton_ong@live.com.sg>
2022-04-01 12:58:59 +1100

    Refactored files into public folder

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/',  function(req,res){
-    res.sendFile(__dirname + '/public/test.html')
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 let server = app.listen(8888, function(){
