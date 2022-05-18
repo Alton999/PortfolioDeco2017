46d9c6fbdc4a39267478bc93a63bf0a73cee4bde
Alton Ong <alton_ong@live.com.sg>
2022-05-18 23:58:14 +1000

    Trouble with refreshing renders

--- dist/index.3b06b0f7.js
+++ dist/index.3b06b0f7.js
@@ -545,8 +545,6 @@ const minutesInput = document.getElementById("minutes");
 const taskDescriptionInput = document.getElementById("description");
 // Task list
 const taskListContainer = document.getElementById("taskListNew");
-// Initialise task list array
-let taskListArray = [];
 // Event listener on the submit form button
 addTaskForm.addEventListener("submit", (e)=>{
     e.preventDefault();
@@ -558,9 +556,10 @@ addTaskForm.addEventListener("submit", (e)=>{
     let minutes = minutesInput.value;
     let description = taskDescriptionInput.value;
     let task = createTaskObject(taskName, priority, difficulty, subject, hours, minutes, description);
-    taskListArray.push(task);
     // Adds the task list array to the local storage object as a key and value pair
-    addToLocalStorage(taskListArray);
+    addToLocalStorage(task);
+    getFromLocalStorage(task.id);
+    // renderTasks(getFromLocalStorage());
     addTaskForm.reset();
 });
 // Function that takes in parameters to create and return 1 task object
@@ -582,121 +581,122 @@ const createTaskObject = (taskName, priority, difficulty, subject, hours, minute
     };
 };
 // Renders the current new task to the DOM
-const renderTasks = (allTasks)=>{
+const renderTask = (task)=>{
     updateEmpty();
+    // console.log("From render", allTasks);
     // Everytime the render tasks function is called we would want to loop over the array and display all values in our local storage
-    allTasks.forEach((task1)=>{
-        // console.log(task);
-        const item = document.createElement("li");
-        item.setAttribute("data-id", task1.id);
-        // Add the css class to the item container
-        item.classList.add("taskListItem");
-        // item.innerHTML = "<p>" + task.taskName + "</p>";
-        // Create task tags div
-        const taskTagsDiv = document.createElement("div");
-        taskTagsDiv.className += "taskTags";
-        // Create task details container
-        const taskDetailsContainer = document.createElement("div");
-        taskDetailsContainer.classList.add("taskDetails");
-        // Manipulate the HTML structure of task deatils container to suit the needs of this list
-        taskDetailsContainer.innerHTML = `
+    console.log(task);
+    const item = document.createElement("li");
+    item.setAttribute("data-id", task.id);
+    // Add the css class to the item container
+    item.classList.add("taskListItem");
+    // item.innerHTML = "<p>" + task.taskName + "</p>";
+    // Create task tags div
+    const taskTagsDiv = document.createElement("div");
+    taskTagsDiv.className += "taskTags";
+    // Create task details container
+    const taskDetailsContainer = document.createElement("div");
+    taskDetailsContainer.classList.add("taskDetails");
+    // Manipulate the HTML structure of task deatils container to suit the needs of this list
+    taskDetailsContainer.innerHTML = `
 			<ul>
 				<li>
 					<p class="label">Task Name:</p>
-					<p>${task1.taskName}</p>
+					<p>${task.taskName}</p>
 				</li>
 				<li>
 					<p class="label">Subject:</p>
-					<p>${task1.subject}</p>
+					<p>${task.subject}</p>
 				</li>
 				<li>
 					<p class="label">Date Created:</p>
-					<p>${task1.createdDate}</p>
+					<p>${task.createdDate}</p>
 				</li>
 				<li>
 					<p class="label">Estimated Duration:</p>
-					<p>${task1.estimatedDuration}</p>
+					<p>${task.estimatedDuration}</p>
 				</li>
 			</ul>
 		`;
-        // Creating the functionality for displaying the tags of priority and difficulty
-        // Create 2 spans to and check the value of each. add class name green, orange or red to change tag color
-        const difficultyTag = document.createElement("span");
-        const priorityTag = document.createElement("span");
-        // Create the text node for the span
-        const difficultyTagText = document.createTextNode(task1.difficulty);
-        const priorityTagText = document.createTextNode(task1.priority);
-        // Add initial class to spans
-        difficultyTag.classList.add("taskTag");
-        priorityTag.classList.add("taskTag");
-        // Append text nodes to span
-        difficultyTag.appendChild(difficultyTagText);
-        priorityTag.appendChild(priorityTagText);
-        // Depending on the colour add classname to spans
-        // Difficulty
-        if (task1.difficulty === "Easy") difficultyTag.classList.add("green");
-        else if (task1.difficulty === "Medium") difficultyTag.classList.add("orange");
-        else difficultyTag.classList.add("red");
-        // Priority
-        if (task1.priority === "Low Priority") priorityTag.classList.add("green");
-        else if (task1.priority === "Medium Priority") priorityTag.classList.add("orange");
-        else priorityTag.classList.add("red");
-        taskTagsDiv.appendChild(difficultyTag);
-        taskTagsDiv.appendChild(priorityTag);
-        // Append the tags into the tasktags div
-        item.appendChild(taskTagsDiv);
-        item.appendChild(taskDetailsContainer);
-        // Creating a delete button associated with specific task
-        let delButton = document.createElement("button");
-        let delButtonText = document.createTextNode("Delete Task");
-        delButton.className += "delButton";
-        delButton.appendChild(delButtonText);
-        item.append(delButton);
-        // // Event listener for additional dom elements
-        delButton.addEventListener("click", (e)=>{
-            e.preventDefault();
-            let id = e.target.parentElement.getAttribute("data-id");
-            let index = taskListArray.findIndex((task)=>task.id === Number(id)
-            );
-            removeItemFromArray(taskListArray, index);
-            item.remove();
-            addToLocalStorage(taskListArray);
-            console.log(taskListArray);
-            updateEmpty();
-        });
-        taskListContainer.appendChild(item);
+    // Creating the functionality for displaying the tags of priority and difficulty
+    // Create 2 spans to and check the value of each. add class name green, orange or red to change tag color
+    const difficultyTag = document.createElement("span");
+    const priorityTag = document.createElement("span");
+    // Create the text node for the span
+    const difficultyTagText = document.createTextNode(task.difficulty);
+    const priorityTagText = document.createTextNode(task.priority);
+    // Add initial class to spans
+    difficultyTag.classList.add("taskTag");
+    priorityTag.classList.add("taskTag");
+    // Append text nodes to span
+    difficultyTag.appendChild(difficultyTagText);
+    priorityTag.appendChild(priorityTagText);
+    // Depending on the colour add classname to spans
+    // Difficulty
+    if (task.difficulty === "Easy") difficultyTag.classList.add("green");
+    else if (task.difficulty === "Medium") difficultyTag.classList.add("orange");
+    else difficultyTag.classList.add("red");
+    // Priority
+    if (task.priority === "Low Priority") priorityTag.classList.add("green");
+    else if (task.priority === "Medium Priority") priorityTag.classList.add("orange");
+    else priorityTag.classList.add("red");
+    taskTagsDiv.appendChild(difficultyTag);
+    taskTagsDiv.appendChild(priorityTag);
+    // Append the tags into the tasktags div
+    item.appendChild(taskTagsDiv);
+    item.appendChild(taskDetailsContainer);
+    // Creating a delete button associated with specific task
+    let delButton = document.createElement("button");
+    let delButtonText = document.createTextNode("Delete Task");
+    delButton.className += "delButton";
+    delButton.appendChild(delButtonText);
+    item.append(delButton);
+    // // Event listener for additional dom elements
+    delButton.addEventListener("click", (e)=>{
+        e.preventDefault();
+        let id = e.target.parentElement.getAttribute("data-id");
+        console.log(id);
+        localStorage.removeItem("Task: " + id.toString());
+        // let index = taskListArray.findIndex((task) => task.id === Number(id));
+        // removeItemFromArray(taskListArray, index);
+        // item.remove();
+        // addToLocalStorage(taskListArray);
+        // console.log(taskListArray);
+        refreshRenders();
+        updateEmpty();
+        // getFromLocalStorage(id);
+        console.log("Button clicked");
     });
+    taskListContainer.appendChild(item);
     addTaskForm.reset();
 };
-const removeItemFromArray = (arr, index)=>{
-    if (index > -1) arr.splice(index, 1);
-    return arr;
-};
 const updateEmpty = ()=>{
-    const reference = localStorage.getItem("allTasks");
-    if (reference) {
-        if (reference.length > 0) document.getElementById("emptyListText").style.display = "none";
-        else document.getElementById("emptyListText").style.display = "block";
+    counter = 0;
+    for(let i = 0; i < localStorage.length; i++){
+        const currentKey = localStorage.key("i");
+        if (currentKey.slice(0, 5) === "Task:") counter += 1;
     }
-};
-const capitaliseFirstLetter = (inputString)=>{
-    return inputString.charAt(0).toUpperCase() + inputString.slice(1);
+    if (counter > 0) document.getElementById("emptyListText").style.display = "none";
+    else document.getElementById("emptyListText").style.display = "block";
 };
 // Add to local storage capability
-const addToLocalStorage = (allTasks)=>{
-    localStorage.setItem("allTasks", JSON.stringify(allTasks));
-    console.log("Task added");
-    renderTasks(allTasks);
+const addToLocalStorage = (task)=>{
+    localStorage.setItem("Task: " + task.id.toString(), JSON.stringify(task));
+    console.log("Task: " + task.id.toString());
 };
-const getFromLocalStorage = ()=>{
-    const reference = localStorage.getItem("allTasks");
-    // Checking if the reference exists
-    if (reference) {
-        allTasks = JSON.parse(reference);
-        renderTasks(allTasks);
-    } else console.log("Key reference not found in local storage");
+const getFromLocalStorage = (taskId)=>{
+    const key = "Task: " + taskId.toString();
+    renderTask(JSON.parse(localStorage.getItem(key)));
 };
-getFromLocalStorage();
+// Refresh local storage renders
+const refreshRenders = ()=>{
+    if (localStorage.length > 0) for(let i = 0; i < localStorage.length; i++){
+        let currentKey = localStorage.key(i);
+        renderTask(JSON.parse(localStorage.getItem(currentKey)));
+    }
+    else updateEmpty();
+};
+refreshRenders();
 
 },{"./addTaskModalComponent":"1Xjui"}],"1Xjui":[function(require,module,exports) {
 // Modal components for add Task
